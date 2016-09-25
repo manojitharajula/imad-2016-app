52a8dcc1046fe8f2b62d382edcd6c73fabe0f116
manojitharajula <manojitharajula@gmail.com>
2016-09-25 21:44:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='changeD';
 var img=document.getElementById('image');
 var marginLeft=0;
-moveRight(){
+function moveRight(){
     magrinLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
